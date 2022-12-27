fb3b4ebbd37ed69755ec9968b5ad53ad0c6f7f60
Rin-t <rin.t.4921@gmail.com>
2022-12-28 00:25:51 +0900

    adapterの作成

---
+++ app/src/main/java/com/example/practicerecyclerview3/RecyclerAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.practicerecyclerview3
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter(private val todoList: ArrayList<ToDoData>): RecyclerView.Adapter<RecyclerAdapter.ViewHolderItem>() {
+
+    inner class ViewHolderItem(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val textViewHolder: TextView = itemView.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderItem {
+        val item = LayoutInflater.from(parent.context).
+        inflate(R.layout.todo_item, parent,false)
+        return  ViewHolderItem(item)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderItem, position: Int) {
+        holder.textViewHolder.text = todoList[position].todo
+    }
+
+    override fun getItemCount(): Int {
+        todoList.size
+    }
+}
